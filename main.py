fd4f35fc10a75945919af57b1f15865bc918f734
Adrian <adrianjennelltiamzon@gmail.com>
2024-11-17 06:55:30 +0800

    Returned To Eventlet Attempted Fix 11172024

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from website import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
+    import eventlet
+    eventlet.monkey_patch()  # Ensure compatibility with Eventlet
     socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
