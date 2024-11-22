ec382f3f02fd0bd11b535b609258326317768114
Adrian <adrianjennelltiamzon@gmail.com>
2024-11-22 20:02:13 +0800

    reduced create_event_edit.html

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from website import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, use_reloader=False)  # use_reloader=False to avoid re-running the app during debug mode
-# 
+    socketio.run(app, debug=True)  # use_reloader=False to avoid re-running the app during debug mode
+# , use_reloader=False
